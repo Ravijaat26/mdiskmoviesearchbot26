2c46e46b1f6dd5e74fa0bd3957989df0c1edbfee
Ravijaat26 <119647661+Ravijaat26@users.noreply.github.com>
2022-12-22 11:25:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
-cd /Mdisk-Search-Bot
+git clone https://github.com/Ravijaat26/mdiskmoviesearchbot26 /mdiskmoviesearchbot26
+cd /mdiskmoviesearchbot26
 pip3 install -r requirements.txt
 echo "Starting Bot....GreyMatter_Bots"
 python3 main.py
